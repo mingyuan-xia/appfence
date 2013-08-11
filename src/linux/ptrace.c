60666b4a37be0d612a151889879adc3ea5f74cd4
Wingcol <djlchr11w1@163.com>
2013-08-12 02:07:22 +0800

    0.1

--- src/linux/ptrace.c
+++ src/linux/ptrace.c
@@ -9,6 +9,7 @@
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <errno.h> 
 
 const int long_size = sizeof(long);
 const int ORIG_EAX = 11;
@@ -35,7 +36,7 @@ void getdata(pid_t child, long addr, char *str)
 			}
 		}
 		if (done){
-			memcpy(laddr, data.chars, j);
+			memcpy(laddr, data.chars, j+1);
 			break;
 		}
 		else {
@@ -45,6 +46,7 @@ void getdata(pid_t child, long addr, char *str)
 		}
 	}
 }
+
 void putdata(pid_t child, long addr, char *str)
 {   
 	char *laddr;
@@ -55,9 +57,9 @@ void putdata(pid_t child, long addr, char *str)
 	}data;
 	int len = strlen(str);
 	i = 0;
-	j = 0;
+	j = len/long_size;
 	laddr = str;
-	while(i < len/long_size) {
+	while(i < j) {
 		memcpy(data.chars, laddr, long_size);
 		ptrace(PTRACE_POKEDATA, child, addr + i * 4, data.val);
 		++i;
@@ -66,30 +68,55 @@ void putdata(pid_t child, long addr, char *str)
 	j = len % long_size;
 	if(j != 0) {
 		memcpy(data.chars, laddr, j);
+		data.chars[j] = 0;
 		ptrace(PTRACE_POKEDATA, child, addr + i * 4, data.val);
 	}
 }
 
-char* chpath(char* orin){
-
+void createPath(char* dir)
+{
+	int	i,len;
+	char str[512];
+	strcpy(str, dir);
+	len = strlen(str);
+	for (i=0; i<len; i++)
+	{
+		if (str[i] == '/')
+		{
+			str[i] = '\0';
+			if (access(str, F_OK) != 0)
+			{
+				mkdir(str, 0777);
+			}
+			str[i] = '/';
+		}
+	}
+	return;
 }
 
-int main(int argc, char *argv[])
-{
-	pid_t traced_process;
-	traced_process = atoi(argv[argc-1]);
-	long orig_eax;
-	long params[2];
-	int status;
-	char *str, *laddr;
-	int toggle = 0;
-	
-	if(0 != ptrace(PTRACE_ATTACH, traced_process, NULL, NULL))
-	{
-		printf("Trace process failed.\n");
-		return 1;
+char* changepath(char* str){
+	char tmp[strlen(str)];
+	strcpy(tmp, str);
+	char* p = strtok(tmp, "/");
+	if ((strcmp(p, "proc") == 0) || (strcmp(p, "run") == 0) || (strcmp(p, "lib") == 0) || (strcmp(p, "dev") == 0) || (strcmp(p, "etc") == 0) || (strcmp(p, "usr") == 0)) {
+		return str;
 	}
-	
+	char* homepath = getenv("HOME");
+	char newPath[256];
+	strcpy (newPath, homepath);
+	strcat (newPath, "/sandbox");
+	strcat(newPath, str);
+	str = newPath;
+	createPath(str);
+	return str;
+}
+
+void trace(pid_t traced_process) {
+	long orig_eax;
+	int status;
+	char *str;
+	int toggle = 0;
+	long params[2];
 	while(1) {
 		wait(&status);
 		if(WIFEXITED(status)) {
@@ -99,19 +126,52 @@ int main(int argc, char *argv[])
 		if(orig_eax == SYS_open) {
 			if(toggle == 0) {
 				toggle = 1;
-				printf("catch SYS_open\n");
 				params[0] = ptrace(PTRACE_PEEKUSER, traced_process, 4 * 0, NULL);
 				params[1] = ptrace(PTRACE_PEEKUSER, traced_process, 4 * 1, NULL);
 				str = (char *)calloc(128, sizeof(char));
 				getdata(traced_process, params[0], str);
 				printf("filepath: %s\n", str);
+				char* newPath = changepath(str);
+				printf("new filepath: %s\n", newPath);
+				putdata(traced_process, params[0], newPath);
 			}
 			else {
 				toggle = 0;
-				printf("exit SYS_open\n\n");
 			}
 		}
 		ptrace(PTRACE_SYSCALL, traced_process, NULL, NULL);
 	}
+}
+
+int main(int argc, char *argv[])
+{
+	pid_t traced_process;
+	if (strcmp(argv[1], "-p") == 0) {
+		traced_process = atoi(argv[2]);	
+		if(0 != ptrace(PTRACE_ATTACH, traced_process, NULL, NULL))
+		{
+			printf("Trace process failed.\n");
+			return 1;
+		}
+		trace(traced_process);
+	}
+	else if (strcmp(argv[1], "-b") == 0) {
+		traced_process = fork();
+		if (traced_process == 0) {
+			ptrace(PTRACE_TRACEME, 0, NULL, NULL);
+			if (execlp(argv[2], NULL) == -1) {
+				if (errno == ENOENT) {
+					printf("filepath error\n");
+				}
+				else if (errno == EACCES) {
+					printf("authority error\n");
+				}
+			}
+		}
+		else {
+			trace(traced_process);
+		}
+	}
+	
 	return 0;
 }
