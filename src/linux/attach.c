f0ebcf947c98f101ff678102d6ffec95700b6565
Robert <chenshyfool@qq.com>
2013-08-14 19:53:01 +0800

    Update attach.c

--- src/linux/attach.c
+++ src/linux/attach.c
@@ -3,7 +3,7 @@
 * and even to intercept fork() in zygote,so that we can perform fork() in
 * our own appfence process.
 *
-* Here is just an example for linux ptrace of using ptrace_attach to set up 
+* Here is just a version for linux ptrace of using ptrace_attach to set up 
 * checkpoints and supplant some instructions.
 *
 * The later version with full functions mentioned above will be work out.
@@ -47,26 +47,23 @@ void getdata(pid_t child, long addr, char *str, int len)
 {   
     char *laddr;
     int i, j;
-    union u 
-    {
-        long val;
-        char chars[long_size];
+    union u {
+             long val;
+             char chars[long_size];
     }data;
     i = 0;
     j = len / long_size;
     laddr = str;
-    while(i < j) 
-    {
-        data.val = ptrace(PTRACE_PEEKDATA, child, addr + i * 4, NULL);
-        memcpy(laddr, data.chars, long_size);
-        ++i;
-        laddr += long_size;
+    while(i < j) {
+                  data.val = ptrace(PTRACE_PEEKDATA, child, addr + i * 4, NULL);
+                  memcpy(laddr, data.chars, long_size);
+                  ++i;
+                  laddr += long_size;
     }
     j = len % long_size;
-    if(j != 0) 
-    {
-        data.val = ptrace(PTRACE_PEEKDATA, child, addr + i * 4, NULL);
-        memcpy(laddr, data.chars, j);
+    if(j != 0) {
+                data.val = ptrace(PTRACE_PEEKDATA, child, addr + i * 4, NULL);
+                memcpy(laddr, data.chars, j);
     }
     str[len] = '\0';
 }
@@ -75,26 +72,23 @@ void putdata(pid_t child, long addr, char *str, int len)
 {   
     char *laddr;
     int i, j;
-    union u 
-    {
-        long val;
-        char chars[long_size];
+    union u {
+             long val;
+             char chars[long_size];
     }data;
     i = 0;
     j = len / long_size;
     laddr = str;
-    while(i < j) 
-    {
-        memcpy(data.chars, laddr, long_size);
-        ptrace(PTRACE_POKEDATA, child, addr + i * 4, data.val);
-        ++i;
-        laddr += long_size;
+    while(i < j) {
+                  memcpy(data.chars, laddr, long_size);
+                  ptrace(PTRACE_POKEDATA, child, addr + i * 4, data.val);
+                  ++i;
+                  laddr += long_size;
     }
     j = len % long_size;
-    if(j != 0) 
-    {
-        memcpy(data.chars, laddr, j);
-        ptrace(PTRACE_POKEDATA, child, addr + i * 4, data.val);
+    if(j != 0) {
+                 memcpy(data.chars, laddr, j);
+                 ptrace(PTRACE_POKEDATA, child, addr + i * 4, data.val);
     }
 }
 
@@ -106,10 +100,9 @@ int main(int argc, char *argv[])
     /* int 0x80, int3 */
     char code[] = {0xcd,0x80,0xcc,0};
     char backup[4];
-    if(argc != 2) 
-    {
-        printf("Usage: %s <pid to be traced>\n", argv[0], argv[1]);
-        exit(1);
+    if(argc != 2) {
+                   printf("Usage: %s <pid to be traced>\n", argv[0], argv[1]);
+                   exit(1);
     }
     traced_process = atoi(argv[1]);
     ptrace(PTRACE_ATTACH, traced_process, NULL, NULL);
