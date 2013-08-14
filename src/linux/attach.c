568c0d57206930f856f1f8d7a8d0a1e06eb10b08
Robert <chenshyfool@qq.com>
2013-08-14 19:48:42 +0800

    Update attach.c

--- src/linux/attach.c
+++ src/linux/attach.c
@@ -19,6 +19,28 @@
 #include <unistd.h>
 #include <linux/user.h>
 
+/*
+int main(int argc, char *argv[])
+{   
+    pid_t traced_process;
+    struct user_regs_struct regs;
+    long ins;
+    if(argc != 2) 
+    {
+        printf("Usage: %s <pid to be traced>\n", argv[0], argv[1]);
+        exit(1);
+    }
+    traced_process = atoi(argv[1]);
+    ptrace(PTRACE_ATTACH, traced_process, NULL, NULL);
+    wait(NULL);
+    ptrace(PTRACE_GETREGS, traced_process, NULL, &regs);
+    ins = ptrace(PTRACE_PEEKTEXT, traced_process, regs.eip, NULL);
+    printf("EIP: %lx Instruction executed: %lx\n", regs.eip, ins);
+    ptrace(PTRACE_DETACH, traced_process, NULL, NULL);
+    return 0;
+}
+*/
+
 const int long_size = sizeof(long);
 
 void getdata(pid_t child, long addr, char *str, int len)
