98d18c86cfb5ec1cdd264ffb3a5ede5511e8ada8
Wingcol <djlchr11w1@163.com>
2013-08-16 01:13:14 +0800

    0.1

---
+++ src/android/chpath.c
@@ -0,0 +1,39 @@
+#include <unistd.h>
+#include <string.h>
+#include <stdio.h>
+
+void createPath(char* dir)
+{
+	int	i,len;
+	char str[512];
+	strcpy(str, dir);
+	len = strlen(str);
+	for (i=0; i<len; i++)
+	{
+		if (str[i] == '/')
+		{
+			str[i] = '\0';
+			if (access(str, F_OK) != 0)
+			{
+				mkdir(str, 0777);
+			}
+			str[i] = '/';
+		}
+	}
+	return;
+}
+
+char* changepath(char* str) 
+{
+	char tmp[strlen(str)];
+	strcpy(tmp, str);
+	char* p = strtok(tmp, "/");
+	if ((strcmp(p, "system") == 0) || (strcmp(p, "sys") == 0) || (strcmp(p, "dev") == 0) || (strcmp(p, "proc") == 0)) {
+		return str;
+	}
+	char newPath[128] = "/data/sandbox";
+	strcat(newPath, str);
+	str = newPath;
+	createPath(str);
+	return str;
+}
