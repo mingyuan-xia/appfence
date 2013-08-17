b2cd42a7f3112490a26046b56b53dfd880d747f8
Robert <chenshyfool@qq.com>
2013-08-17 11:57:12 +0800

    Update attach.c

--- src/android/component2/attach.c
+++ src/android/component2/attach.c
@@ -14,35 +14,15 @@
 * attach
 */
 
+#include <stdio.h>
+
 #include <sys/ptrace.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h>
-#include <linux/user.h>
-#include <sys/syscall.h>   /* For SYS_FORK etc */
+#include <sys/user.h>
+#include <sys/syscall.h>   /* For SYS_fork etc */
 
-/*
-int main(int argc, char *argv[])
-{   
-    pid_t traced_process;
-    struct user_regs_struct regs;
-    long ins;
-    
-    if(argc != 2) {
-        printf("Usage: %s <pid to be traced>\n", argv[0], argv[1]);
-        exit(1);
-    }
-    traced_process = atoi(argv[1]);
-    ptrace(PTRACE_ATTACH, traced_process, NULL, NULL);
-    wait(NULL);
-    ptrace(PTRACE_GETREGS, traced_process, NULL, &regs);
-    ins = ptrace(PTRACE_PEEKTEXT, traced_process, regs.eip, NULL);
-    printf("EIP: %lx Instruction executed: %lx\n", regs.eip, ins);
-    ptrace(PTRACE_DETACH, traced_process, NULL, NULL);
-    return 0;
-}*/
-
-// First try other SYS_CALLs
 #define ORIG_EAX 11
 #define EBX 0
 #define ECX 1
@@ -51,34 +31,36 @@ int main(int argc, char *argv[])
 
 int main(int argc, char * argv[])
 {   
-    int pid_t = 33;
+    pid_t child;
     long orig_eax, eax;
     long params[3];
     int status;
     int insyscall = 0;
-
+ 
     if(argc < 2){
         printf("need pid\n");
         return 0;
     }
 
+
+    child = atoi(argv[1]);
     printf("try to attch pid [%d]\n", child);
-    ptrace(PTRACE_ATTACH, pid_t, 0, 0);
+    ptrace(PTRACE_ATTACH, child, 0, 0);
 
     int count = 20;
     while(count-- > 0) {
         wait(&status);
         printf("recv sig [%d]\n", WSTOPSIG(status));
         if(WIFEXITED(status)){
-            printf("The target has dead\n");
-            break;
+             printf("The child has been dead\n");
+             break;
         }
         struct user_regs_struct regs;
-        ptrace(PTRACE_GETREGS, pid_t, NULL, &regs);
+        ptrace(PTRACE_GETREGS, child, NULL, &regs);
         printf("EIP: %lx , orig_eax %lx, eax %lx\n", regs.eip, regs.orig_eax, regs.eax);
 
         printf("PTRACE_SYSCALL\n");
-        ptrace(PTRACE_SYSCALL, pid_t, NULL, NULL);
+        ptrace(PTRACE_SYSCALL, child, NULL, NULL);
 
         printf("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n\n");
     }
@@ -146,56 +128,6 @@ int main(int argc, char *argv[])
     return 0;
 }*/
 
-/*
-* Try to find the entry and the exit of fork() in zygote
-*
-void __cyg_profile_func_enter (void *this_fn, void *call_site)
-{
-    int indent;
-
-    if (stack_at == stack_size){
-        unsigned size_new = stack_size ? stack_size * 2 : 0x100;
-        struct stack *stack_new;
-
-        stack_new = realloc (stack, sizeof (*stack) * size_new);
-        if (!stack_new){
-	         fprintf (stderr, PREFIX "stack size %u allocation failure!\n", size_new);
-	         return;
-	    }
-        stack = stack_new;
-        stack_size = size_new;
-    }
-    stack[stack_at].this_fn = this_fn;
-    stack[stack_at].call_site = call_site;
-    stack_at++;
-
-    indent = stack_at - 1 <= INDENT_MAX ? stack_at - 1 : INDENT_MAX;
-
-    fprintf (stderr, PREFIX "%*s->%s", indent, "", addr_print (this_fn));
-    fprintf (stderr, " (%s)\n", addr_print (call_site));
-}
-
-void __cyg_profile_func_exit  (void *this_fn, void *call_site)
-{
-    int indent;
-
-    indent = stack_at - 1 <= INDENT_MAX ? stack_at - 1 : INDENT_MAX;
-
-    fprintf (stderr, PREFIX "%*s<-%s", indent, "", addr_print (this_fn));
-    fprintf (stderr, " (%s)\n", addr_print (call_site));
-
-    if (!stack_at){
-        fprintf (stderr, PREFIX "stack empty!\n");
-        return;
-    }
-    if (stack[stack_at - 1].this_fn != this_fn {
-        || stack[stack_at - 1].call_site != call_site)
-        fprintf (stderr, PREFIX "Unexpected function leave, resetting the stack\n");
-        stack_at = 0;
-        return;
-    }
-    stack_at--;
-}*/
 
 /*
 void putdata(pid_t child, long addr, char *str, int len)
@@ -227,41 +159,4 @@ void putdata(pid_t child, long addr, char *str, int len)
     }
 }
 
-int main(int argc, char *argv[])
-{   
-    pid_t traced_process;
-    struct user_regs_struct regs, newregs;
-    long ins;
-    // int 0x80, int3 
-    char code[] = {0xcd,0x80,0xcc,0};
-    char backup[4];
-    if(argc != 2) {
-        printf("Usage: %s <pid to be traced>\n", argv[0], argv[1]);
-        exit(1);
-    }
-    traced_process = atoi(argv[1]);
-    
-    ptrace(PTRACE_ATTACH, traced_process, NULL, NULL);
-    wait(NULL);
-    ptrace(PTRACE_GETREGS, traced_process,NULL, &regs);
-    
-    // Copy instructions into a backup variable 
-    getdata(traced_process, regs.eip, backup, 3);
-    
-    // Put the breakpoint 
-    putdata(traced_process, regs.eip, code, 3);
-    
-    // Let the process continue and execute the int 3 instruction 
-    ptrace(PTRACE_CONT, traced_process, NULL, NULL);
-    wait(NULL);
-    
-    printf("The process stopped, putting back the original instructions\n");
-    printf("Press <enter> to continue\n");
-    getchar();
-    
-    putdata(traced_process, regs.eip, backup, 3);
-    // Setting the eip back to the original instruction to let the process continue 
-    ptrace(PTRACE_SETREGS, traced_process, NULL, &regs);
-    ptrace(PTRACE_DETACH, traced_process, NULL, NULL);
-    return 0;
-}*/
+
