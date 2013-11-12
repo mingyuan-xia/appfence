b702acc6d438232599df13f066866be9d25bf317
yx <qqfishyx@gmail.com>
2013-11-13 01:22:27 +0800

    only trace the open syscall with WRITE flags

--- jni/sandbox_helper.c
+++ jni/sandbox_helper.c
@@ -4,7 +4,7 @@
  */
 #include <sys/ptrace.h>
 #include <sys/syscall.h>
-
+#include <fcntl.h>
 #include <sys/wait.h>
 #include <signal.h>
 #include <assert.h>
@@ -13,7 +13,11 @@
 #include "sandbox_helper.h"
 #include "ptraceaux.h"
 
-pid_t ptrace_app_process(pid_t pid){
+#define IS_WRITE(oflag) ((oflag & O_WRONLY) != 0 || (oflag & O_RDWR) != 0)
+
+
+pid_t ptrace_app_process(pid_t pid)
+{
 	if (ptrace_attach(pid)) {
 		return -1;
 	}
@@ -27,13 +31,18 @@ pid_t ptrace_app_process(pid_t pid){
 		switch(syscall_no){
 			case __NR_open:
 			{
-				long arg0 = ptrace_tool.ptrace_get_syscall_arg(pid, 0);
-				int len = ptrace_strlen(pid, (void*) arg0);
-				char path[len + 1];
-				ptrace_tool.ptrace_read_data(pid, path, (void *)arg0, len);
-				path[len] = 0;
-				/* ptrace_write_data(pid, path, (void*)regs.ARM_r1, regs.ARM_r2+1); */
-				printf("pid %d open: %s\n",pid, path);
+				//arg1 is oflag
+				long arg1 = ptrace_tool.ptrace_get_syscall_arg(pid, 1);
+				if(IS_WRITE(arg1)){
+					//first arg of open is path addr
+					long arg0 = ptrace_tool.ptrace_get_syscall_arg(pid, 0);
+					int len = ptrace_strlen(pid, (void*) arg0);
+					char path[len + 1];
+					ptrace_tool.ptrace_read_data(pid, path, (void *)arg0, len);
+					path[len] = 0;
+					/* ptrace_write_data(pid, path, (void*)regs.ARM_r1, regs.ARM_r2+1); */
+					printf("pid %d open: %s\n",pid, path);
+				}
 				break;
 			}
 		}
